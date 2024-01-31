5b244f2df27061e451125f49a5f5e81a444a5cfa
rjranjan2112 <rjranjan2112@gmail.com>
2024-01-31 19:00:43 +0530

    Use of Function argument

---
+++ Function_Arguments.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void calculatePrice(float value);
+
+int main(){
+    float value = 100.3;
+    calculatePrice( value);
+    return 0;
+}
+
+void calculatePrice(float value){
+    value=value+(0.18 * value);
+    printf("Final Price is %f",value);
+
+}
