b11089fdfb19b5bfaec38685144c67079afdffb9
rjranjan2112 <rjranjan2112@gmail.com>
2024-01-31 18:06:44 +0530

    Basic Syntax of Function

---
+++ Function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+//Function Declaration
+void printHello();
+
+//Function Calling
+int main() {
+    printHello();
+    return 0;
+}
+
+//Function Definition
+void printHello(){
+    printf("Hello Function Calling");
+}
