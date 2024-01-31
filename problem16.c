94512d7791457bdf297cc5ad9dc46bd9f90587ab
rjranjan2112 <rjranjan2112@gmail.com>
2024-01-31 16:23:03 +0530

    Sum of numbers in loop

---
+++ problem16.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i,sum;
+    for (i = 5; i<= 50; i++){
+        printf("%d\n",i);
+        sum=sum+i;
+        }
+        printf("Answer = %d\n",sum);
+    return 0;
+}
