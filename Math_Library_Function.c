ea73974315512cc81a4c8131e7f1aa71459999d9
rjranjan2112 <rjranjan2112@gmail.com>
2024-01-31 19:12:21 +0530

    Power Calculation

---
+++ Math_Library_Function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n,ans;
+    printf("Enter the Number: ");
+    scanf("%d",&n);
+    ans = pow(n,2);
+
+    printf("The power of number is = %d\n",ans);
+    return 0;
+
+}
