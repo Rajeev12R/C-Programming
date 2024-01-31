f36f83d8de5b091ab8d078576152e7387b21ac75
rjranjan2112 <rjranjan2112@gmail.com>
2024-01-31 18:14:43 +0530

    Use of Function

---
+++ problem17.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+    void printHello(),printGoodbye();
+
+    int main(){
+
+        printHello();
+        printGoodbye();
+        return 0;
+    }
+void printHello(){
+    printf("Hello I am Rajeev\n");
+}
+void printGoodbye(){
+    printf("Bye bye nice to meet you Rajeev");
+}
