b5aa1b656e42f11eda9d295b9c6aac7e6a7ed333
rjranjan2112 <rjranjan2112@gmail.com>
2024-01-31 12:30:05 +0530

    Pre and Post Increment_Decrement

---
+++ Increment_Decrement.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+
+//  Post increment  -->
+    int i =1;
+    printf("%d \n", i++);
+    printf("%d \n",i);
+
+//  Pre increment  -->
+    int a=1;
+    printf("%d \n", ++a);
+    printf("%d \n", a);
+
+//  Post decrement  -->
+    int j =1;
+    printf("%d \n", j--);
+    printf("%d \n",j);
+
+//  Pre increment  -->
+    int b=1;
+    printf("%d \n", --b);
+    printf("%d \n", b);
+
+
+    return 0;
+}
