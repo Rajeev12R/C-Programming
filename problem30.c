c3e43323e36dd227fc2f1d438dff8a0d4432324e
rjranjan2112 <rjranjan2112@gmail.com>
2024-02-13 12:00:39 +0530

    If else problem

---
+++ problem30.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    if (ch == 'a'|| ch == 'e' || ch == 'o'||ch == 'i'||ch == 'o'||ch == 'u'|| ch == 'A'|| ch == 'E'||ch == 'I'||ch == 'O'||ch == 'U')
+    printf("Vowel");
+    else
+    printf("Not vowel");
+    return 0;
+}
