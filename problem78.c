ef37f5a9851a0079e1d88eb972c6c7f88a6a5ffd
rjranjan2112 <rjranjan2112@gmail.com>
2024-04-10 14:38:28 +0530

    pointer with array

---
+++ problem78.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+
+int arr[]={1,2,3,4,5,6,7,8,9};
+int *p1,*p2;
+
+p1 =arr;
+p1++;
+printf("\n %d",*p1);
+p1--;
+printf("\n %d",*p1);
+p1 = p1+2;
+printf("\n %d",*p1);
+p1 = p1-2;
+printf("\n %d",*p1);
+p2 = &arr[4];
+printf("\n %d\n",p2-p1);
+while(p1 <= p2){
+    printf("%d ",*p1);
+    p1++;
+}
+
+
+
+
+return 0;
+}
